137dac406692ef495321985c69d071626c8cbb4f
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-12-22 20:32:22 -0300

    fix: getRanking with left join fixed

--- src/repositories/ranking.repository.js
+++ src/repositories/ranking.repository.js
@@ -6,8 +6,8 @@ export function selectRanking(){
         users.id,
         users.name,
         COUNT(DISTINCT urls.id) AS "linksCount",
-        SUM(urls."visitCount") AS "visitCount"
-    FROM users JOIN urls ON users.id = urls."userId"
+        COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
+    FROM users LEFT JOIN urls ON users.id = urls."userId"
     GROUP BY users.id
     ORDER BY "visitCount" DESC LIMIT 10`);
 }
